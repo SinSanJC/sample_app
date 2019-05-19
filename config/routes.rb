305071bb34cbc7026cf99ed5d4762b7b1e522d21
Adrian <sinsan-ajc@hotmail.com>
2019-05-19 21:43:15 +0200

    Add SignUp

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   resources :users
-  get 'users/new'
 
 	root 'static_pages#home'
 	get  '/help',   to: 'static_pages#help'
@@ -8,6 +7,5 @@ Rails.application.routes.draw do
 	get  '/contact',   to: 'static_pages#contact'
 	
 	get  '/signup',  to: 'users#new'
-	
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
