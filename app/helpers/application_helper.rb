f064a8b6ae809d9711bd4cc76e3821c2863515ef
Adrian <sinsan-ajc@hotmail.com>
2019-05-22 21:24:00 +0200

    Edit design

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,16 @@
 module ApplicationHelper
-  def full_title(page_title = '')
-    base_title = "Adrian"
-    if page_title.empty?
-      base_title
-    else
-      page_title + " | " + base_title
-    end
-  end
+	def full_title(page_title = '')
+		base_title = "Adrian"
+		if page_title.empty?
+			base_title
+		else
+			page_title + " | " + base_title
+		end
+	end
+
+	def current?(key, path)
+		"#{key}" if current_page? path
+	end
+
+
 end
