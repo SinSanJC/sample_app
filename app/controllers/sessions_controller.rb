5b1c1f09021fa2b5613faf2a4bfb1a2b587e1e54
Adrian <sinsan-ajc@hotmail.com>
2019-05-22 13:32:21 +0200

    Add Login/Logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,27 @@
 class SessionsController < ApplicationController
-  def new
-  end
+	def new
+	end
+
+	def create
+		user = User.find_by(email: params[:session][:email].downcase)
+		if !params[:session][:email].blank? && !params[:session][:email].blank?
+			if !user.nil? && user.authenticate(params[:session][:password])
+				log_in user
+				redirect_to user
+			else
+				params[:session][:email] = params[:session][:email].downcase
+				flash.now[:danger] = 'Invalid email/password combination'
+				render 'new'
+			end
+		else
+			params[:session][:email] = params[:session][:email].downcase
+			flash.now[:danger] = 'Insert user and password'
+			render 'new'
+		end
+	end
+
+	def destroy
+		log_out
+		redirect_to root_url
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
+	get 'sessions/new'
 
-  resources :users
+	resources :users
 
 	root 'static_pages#home'
 	get  '/help',   to: 'static_pages#help'
@@ -9,5 +9,10 @@ Rails.application.routes.draw do
 	get  '/contact',   to: 'static_pages#contact'
 	
 	get  '/signup',  to: 'users#new'
+	get '/login',   to: 'sessions#new'
+	post '/login',   to: 'sessions#create'
+	delete '/logout',  to: 'sessions#destroy'
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
